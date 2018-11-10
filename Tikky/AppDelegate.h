2a03cc56ba0bac699947ebd6c5199cb09f7100a8
Lê Hoàng Vũ <elhoangvu@gmail.com>
2018-11-10 14:54:32 +0700

    Initial Commit

---
+++ Tikky/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tikky
+//
+//  Created by Le Hoang Vu on 11/10/18.
+//  Copyright © 2018 Le Hoang Vu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tikky/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tikky
+//
+//  Created by Le Hoang Vu on 11/10/18.
+//  Copyright © 2018 Le Hoang Vu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
